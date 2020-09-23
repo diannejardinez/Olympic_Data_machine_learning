57ccebc4cd4f47146d92c0b58069c25fd11a687a
diannejardinez <45705176+diannejardinez@users.noreply.github.com>
2020-09-22 23:33:19 -0700

    fixing pathways

---
+++ Flask-API/static/js/-test-app_chart_1.js
@@ -0,0 +1,280 @@
+// Setting events
+var xValue = ["Basketball Men's Basketball", "Boxing Men's Bantamweight",
+       "Boxing Men's Featherweight", "Boxing Men's Flyweight",
+       "Boxing Men's Heavyweight", "Boxing Men's Light-Flyweight",
+       "Boxing Men's Light-Heavyweight",
+       "Boxing Men's Light-Middleweight",
+       "Boxing Men's Light-Welterweight", "Boxing Men's Lightweight",
+       "Boxing Men's Middleweight", "Boxing Men's Super-Heavyweight",
+       "Boxing Men's Welterweight",
+       "Cycling Men's 1,000 metres Time Trial",
+       "Cycling Men's 100 kilometres Team Time Trial",
+       "Cycling Men's BMX",
+       "Cycling Men's Individual Pursuit, 4,000 metres",
+       "Cycling Men's Individual Time Trial", "Cycling Men's Keirin",
+       "Cycling Men's Madison",
+       "Cycling Men's Mountainbike, Cross-Country",
+       "Cycling Men's Omnium", "Cycling Men's Points Race",
+       "Cycling Men's Road Race, Individual",
+       "Cycling Men's Road Race, Team", "Cycling Men's Sprint",
+       "Cycling Men's Tandem Sprint, 2,000 metres",
+       "Cycling Men's Team Pursuit, 4,000 metres",
+       "Cycling Men's Team Sprint", "Figure Skating Men's Singles",
+       'Figure Skating Mixed Ice Dancing', 'Figure Skating Mixed Pairs',
+       'Figure Skating Mixed Team', "Gymnastics Men's Floor Exercise",
+       "Gymnastics Men's Horizontal Bar",
+       "Gymnastics Men's Horizontal Bar, Teams",
+       "Gymnastics Men's Horse Vault",
+       "Gymnastics Men's Individual All-Around",
+       "Gymnastics Men's Parallel Bars",
+       "Gymnastics Men's Parallel Bars, Teams",
+       "Gymnastics Men's Pommelled Horse", "Gymnastics Men's Rings",
+       "Gymnastics Men's Rope Climbing",
+       "Gymnastics Men's Team All-Around",
+       "Gymnastics Men's Team All-Around, Swedish System",
+       "Judo Men's Extra-Lightweight", "Judo Men's Half-Heavyweight",
+       "Judo Men's Half-Lightweight", "Judo Men's Half-Middleweight",
+       "Judo Men's Heavyweight", "Judo Men's Lightweight",
+       "Judo Men's Middleweight", "Judo Men's Open Class",
+       "Rowing Men's Coxed Eights", "Rowing Men's Coxed Fours",
+       "Rowing Men's Coxed Pairs", "Rowing Men's Coxless Fours",
+       "Rowing Men's Coxless Pairs", "Rowing Men's Double Sculls",
+       "Rowing Men's Lightweight Coxless Fours",
+       "Rowing Men's Lightweight Double Sculls",
+       "Rowing Men's Quadruple Sculls", "Rowing Men's Single Sculls",
+       "Snowboarding Men's Boardercross",
+       "Snowboarding Men's Giant Slalom", "Snowboarding Men's Halfpipe",
+       "Snowboarding Men's Parallel Giant Slalom",
+       "Snowboarding Men's Parallel Slalom",
+       "Snowboarding Men's Slopestyle",
+       "Speed Skating Men's 1,000 metres",
+       "Speed Skating Men's 1,500 metres",
+       "Speed Skating Men's 10,000 metres",
+       "Speed Skating Men's 5,000 metres",
+       "Speed Skating Men's 500 metres", "Speed Skating Men's Allround",
+       "Speed Skating Men's Team Pursuit (8 laps)",
+       "Swimming Men's 1,500 metres Freestyle",
+       "Swimming Men's 10 kilometres Open Water",
+       "Swimming Men's 100 metres Backstroke",
+       "Swimming Men's 100 metres Breaststroke",
+       "Swimming Men's 100 metres Butterfly",
+       "Swimming Men's 100 metres Freestyle",
+       "Swimming Men's 100 yard Freestyle",
+       "Swimming Men's 200 metres Backstroke",
+       "Swimming Men's 200 metres Breaststroke",
+       "Swimming Men's 200 metres Butterfly",
+       "Swimming Men's 200 metres Freestyle",
+       "Swimming Men's 200 metres Individual Medley",
+       "Swimming Men's 220 yard Freestyle",
+       "Swimming Men's 4 x 100 metres Freestyle Relay",
+       "Swimming Men's 4 x 100 metres Medley Relay",
+       "Swimming Men's 4 x 200 metres Freestyle Relay",
+       "Swimming Men's 4 x 250 metres Freestyle Relay",
+       "Swimming Men's 4 x 50 Yard Freestyle Relay",
+       "Swimming Men's 400 metres Freestyle",
+       "Swimming Men's 400 metres Individual Medley",
+       "Swimming Men's 440 yard Freestyle",
+       "Swimming Men's 50 metres Freestyle",
+       "Swimming Men's 50 yard Freestyle",
+       "Swimming Men's One Mile Freestyle", "Tennis Men's Doubles",
+       "Tennis Men's Singles", 'Tennis Mixed Doubles',
+       "Volleyball Men's Volleyball", "Weightlifting Men's Bantamweight",
+       "Weightlifting Men's Featherweight",
+       "Weightlifting Men's Flyweight", "Weightlifting Men's Heavyweight",
+       "Weightlifting Men's Heavyweight I",
+       "Weightlifting Men's Heavyweight II",
+       "Weightlifting Men's Light-Heavyweight",
+       "Weightlifting Men's Lightweight",
+       "Weightlifting Men's Middle-Heavyweight",
+       "Weightlifting Men's Middleweight",
+       "Weightlifting Men's Super-Heavyweight",
+       "Weightlifting Men's Unlimited, One Hand",
+       "Wrestling Men's Bantamweight, Freestyle",
+       "Wrestling Men's Bantamweight, Greco-Roman",
+       "Wrestling Men's Featherweight, Freestyle",
+       "Wrestling Men's Featherweight, Greco-Roman",
+       "Wrestling Men's Flyweight, Freestyle",
+       "Wrestling Men's Flyweight, Greco-Roman",
+       "Wrestling Men's Heavyweight, Freestyle",
+       "Wrestling Men's Heavyweight, Greco-Roman",
+       "Wrestling Men's Light-Flyweight, Freestyle",
+       "Wrestling Men's Light-Flyweight, Greco-Roman",
+       "Wrestling Men's Light-Heavyweight, Freestyle",
+       "Wrestling Men's Light-Heavyweight, Greco-Roman",
+       "Wrestling Men's Lightweight, Freestyle",
+       "Wrestling Men's Lightweight, Greco-Roman",
+       "Wrestling Men's Middleweight, Freestyle",
+       "Wrestling Men's Middleweight, Greco-Roman",
+       "Wrestling Men's Super-Heavyweight, Freestyle",
+       "Wrestling Men's Super-Heavyweight, Greco-Roman",
+       "Wrestling Men's Unlimited Class, Greco-Roman",
+       "Wrestling Men's Welterweight, Freestyle",
+       "Wrestling Men's Welterweight, Greco-Roman"];
+
+
+// Setting yvalue
+var yValue1 = [25.0, 22.0, 20.0, 22.0, 24.0, 23.0, 23.0, 23.0, 23.0, 24.0, 23.0, 24.0, 24.0, 24.0, 22.0, 23.0, 24.0, 33.0, 28.0, 30.0, 27.0, 24.0, 27.0, 26.0, 21.0, 24.0, 24.0, 24.0, 26.0, 22.0, 29.0, 27.0, 30.0, 22.0, 25.0, 26.0, 24.0, 23.0, 25.0, 26.0, 23.0, 24.0, 22.0, 23.0, 26.0, 24.0, 24.0, 26.0, 23.0, 26.0, 23.0, 26.0, 28.0, 25.0, 24.0, 25.0, 26.0, 26.0, 27.0, 27.0, 28.0, 24.0, 26.0, 29.0, 26.0, 22.0, 26.0, 27.0, 20.0, 25.0, 26.0, 24.0, 27.0, 24.0, 30.0, 24.0, 20.0, 27.0, 21.0, 23.0, 22.0, 22.0, 23.0, 21.0, 21.0, 22.0, 20.0, 22.0, 19.0, 22.0, 22.0, 21.0, 24.0, 19.0, 20.0, 21.0, 19.0, 22.0, 23.0, 21.0, 26.0, 26.0, 29.0, 27.0, 25.0, 25.0, 23.0, 24.0, 27.0, 23.0, 26.0, 24.0, 25.0, 24.0, 26.0, 24.0, 26.0, 24.0, 25.0, 26.0, 22.0, 26.0, 26.0, 28.0, 23.0, 23.0, 24.0, 25.0, 24.0, 24.0, 27.0, 26.0, 27.0, 26.0, 26.0, 25.0, 26.0]
+var yValue2 = [6.496062992125984, 5.429790026246719, 5.511811023622047, 5.413385826771654, 6.233595800524935, 5.413385826771654, 6.053149606299213, 5.774278215223097, 5.610236220472441, 5.57742782152231, 5.971128608923885, 6.381233595800524, 5.774278215223097, 5.905511811023622, 5.905511811023622, 6.102362204724409, 6.069553805774278, 6.102362204724409, 5.971128608923885, 5.807086614173229, 5.905511811023622, 5.856299212598425, 5.83989501312336, 5.83989501312336, 5.971128608923885, 6.003937007874016, 5.708661417322834, 5.971128608923885, 5.8727034120734904, 5.8727034120734904, 5.823490813648294, 5.83989501312336, 6.003937007874016, 5.396981627296588, 5.479002624671916, 5.216535433070866, 5.429790026246719, 5.347769028871391, 5.347769028871391, 5.216535433070866, 5.544619422572178, 5.380577427821522, 5.479002624671916, 5.347769028871391, 5.774278215223097, 5.380577427821522, 6.069553805774278, 5.380577427821522, 5.889107611548557, 6.069553805774278, 5.6430446194225725, 5.938320209973753, 5.905511811023622, 6.266404199475065, 6.217191601049869, 6.135170603674541, 6.299212598425196, 6.299212598425196, 6.167979002624672, 6.053149606299213, 5.9547244094488185, 6.364829396325459, 6.200787401574803, 6.069553805774278, 5.83989501312336, 5.741469816272966, 5.938320209973753, 5.8727034120734904, 5.83989501312336, 6.069553805774278, 5.98753280839895, 6.036745406824147, 6.069553805774278, 5.905511811023622, 5.479002624671916, 6.020341207349081, 6.069553805774278, 6.299212598425196, 6.233595800524935, 6.003937007874016, 6.102362204724409, 6.266404199475065, 6.135170603674541, 6.200787401574803, 6.069553805774278, 6.167979002624672, 6.233595800524935, 6.266404199475065, 6.003937007874016, 6.233595800524935, 6.167979002624672, 6.167979002624672, 6.135170603674541, 6.003937007874016, 6.069553805774278, 6.1515748031496065, 6.003937007874016, 6.496062992125984, 6.135170603674541, 5.413385826771654, 6.069553805774278, 6.233595800524935, 6.3484251968503935, 6.397637795275591, 4.986876640419948, 5.118110236220472, 5.019685039370079, 5.905511811023622, 5.807086614173229, 5.971128608923885, 5.6430446194225725, 5.413385826771654, 5.708661417322834, 5.561023622047244, 6.069553805774278, 5.98753280839895, 5.413385826771654, 5.31496062992126, 5.511811023622047, 5.479002624671916, 5.282152230971128, 5.282152230971128, 6.069553805774278, 6.069553805774278, 5.150918635170604, 5.2493438320209975, 5.905511811023622, 5.807086614173229, 5.57742782152231, 5.57742782152231, 5.741469816272966, 5.774278215223097, 6.233595800524935, 6.332020997375328, 5.216535433070866, 5.610236220472441, 5.675853018372703]
+var yValue3 = [209.475, 119.07000000000001, 125.685, 112.455, 200.655, 105.84, 178.60500000000002, 156.555, 141.12, 132.3, 165.375, 222.15375, 147.735, 171.99, 164.2725, 198.45000000000002, 175.2975, 176.4, 198.45000000000002, 158.76, 149.94, 157.6575, 156.555, 158.76, 158.76, 198.45000000000002, 160.965, 165.375, 180.81, 154.35, 158.76, 166.4775, 163.17000000000002, 131.1975, 141.12, 154.35, 137.8125, 130.095, 130.095, 154.35, 134.505, 132.3, 130.095, 130.095, 160.965, 132.3, 220.5, 145.53, 175.2975, 275.625, 160.965, 189.63, 245.85750000000002, 196.245, 196.245, 194.04000000000002, 200.655, 198.45000000000002, 191.835, 158.76, 162.0675, 205.065, 202.86, 200.655, 178.60500000000002, 154.35, 195.1425, 180.81, 165.375, 189.63, 173.0925, 180.81, 176.4, 171.99, 147.735, 176.4, 166.4775, 189.63, 186.32250000000002, 176.4, 185.22, 176.4, 183.01500000000001, 185.22, 169.785, 176.4, 171.99, 189.63, 154.35, 185.22, 185.22, 178.60500000000002, 183.01500000000001, 154.35, 174.195, 183.01500000000001, 154.35, 191.835, 183.01500000000001, 145.53, 176.4, 180.81, 191.835, 196.245, 123.48, 136.71, 119.07000000000001, 235.935, 211.68, 246.96, 183.01500000000001, 152.145, 205.065, 165.375, 341.77500000000003, 238.14000000000001, 125.685, 125.685, 136.71, 136.71, 123.48, 120.1725, 220.5, 220.5, 105.84, 105.84, 195.1425, 192.38625000000002, 149.94, 149.94, 180.81, 180.81, 264.6, 259.08750000000003, 154.35, 162.0675, 164.2725]
+
+// Setting textvalue 
+var textValue1 = ('Male Age')
+var textValue2 = ('Male Height')
+var textValue3 = ('Male Weight')
+
+
+function barChart1() {
+  var barChart = {
+    name: "Gold",
+    x: xValue,
+    y: yValue1,
+    type: 'bar',
+    text: yValue1.map(String),
+    textposition: 'auto',
+    hoverinfo: true,
+    opacity: 0.5,
+    marker: {
+      color: 'rgba(218,165,32,.7)',
+      line: {
+        color: 'rgba(218,165,32,1)',
+        width: 1.5
+      }
+    }
+  };
+
+  var layout = {
+    title: {
+      text: textValue1,
+      font: {
+        size: 20,
+      }},
+    autosize: true,
+    width: 1100,
+    height: 800,
+    margin: {
+      l: 50,
+      r: 50,
+      b: 300,
+      t: 100,
+      pad: 4},
+    xaxis: {
+      categoryorder:'total descending',
+      title: {
+        text: '',
+        font: {
+          size: 20,
+        }}},
+    yaxis: {
+        range: [0, 35]
+        },
+
+  };
+
+  Plotly.newPlot("plot1", [barChart], layout);
+}
+
+
+
+function barChart2() {
+  var barChart = {
+    name: "Gold",
+    x: xValue,
+    y: yValue2,
+    type: 'bar',
+    text: yValue2.map(String),
+    textposition: 'auto',
+    hoverinfo: true,
+    opacity: 0.5,
+    marker: {
+      color: 'rgba(218,165,32,.7)',
+      line: {
+        color: 'rgba(218,165,32,1)',
+        width: 1.5
+      }
+    }
+  };
+
+  var layout = {
+    title: {
+      text: textValue2,
+      font: {
+        size: 20,
+      }},
+    autosize: true,
+    width: 1100,
+    height: 800,
+    margin: {
+      l: 50,
+      r: 50,
+      b: 300,
+      t: 100,
+      pad: 4},
+    xaxis: {
+      categoryorder:'total descending',
+      title: {
+        text: '',
+        font: {
+          size: 20,
+        }}},
+    yaxis: {
+        range: [0, 7]
+        },
+
+  };
+
+  Plotly.newPlot("plot2", [barChart], layout);
+}
+
+function barChart3() {
+  var barChart = {
+    name: "Gold",
+    x: xValue,
+    y: yValue3,
+    type: 'bar',
+    text: yValue3.map(String),
+    textposition: 'auto',
+    hoverinfo: true,
+    opacity: 0.5,
+    marker: {
+      color: 'rgba(218,165,32,.7)',
+      line: {
+        color: 'rgba(218,165,32,1)',
+        width: 1.5
+      }
+    }
+  };
+
+  var layout = {
+    title: {
+      text: textValue3,
+      font: {
+        size: 20,
+      }},
+    autosize: true,
+    width: 1100,
+    height: 800,
+    margin: {
+      l: 50,
+      r: 50,
+      b: 300,
+      t: 100,
+      pad: 4},
+    xaxis: {
+      categoryorder:'total descending',
+      title: {
+        text: '',
+        font: {
+          size: 20,
+        }}},
+    yaxis: {
+        range: [0, 350]
+        },
+
+  };
+
+  Plotly.newPlot("plot3", [barChart], layout);
+}
+
+barChart1();
+barChart2();
+barChart3();
---
+++ Flask-API/static/js/ORIGINAL - app_chart_1.js
@@ -0,0 +1,330 @@
+
+
+// // Setting json files to a variable
+// var promises = [
+//   d3.json("/api/event/body-composition/F"),
+//   d3.json("/api/event/body-composition/M")]
+
+// // Loading multiple datasets 
+// Promise.all(promises).then(function(allData){
+//   var femaleData = allData[0];
+//   var maleData = allData[1];
+//   // console.log(femaleData)
+//   // console.log(maleData)
+
+
+// // Loading static json files
+// d3.json("static/assets/data/male_bodycomp_age.json").then((maleData) => {
+//   console.log(maleData)
+// }); 
+
+// d3.json("static/assets/data/female_bodycomp_age.json").then((femaleData) => {
+//   console.log(femaleData)
+// }); 
+
+var femaleData = [d3.json("static/assets/data/female_bodycomp_age.json")]
+var maleData = [d3.json("static/assets/data/male_bodycomp_age.json")]
+console.log(maleData)
+
+// d3.json("static/assets/data/female_bodycomp_age.json", function(femaleData) {
+//   console.log(maleData)
+// });
+
+// d3.json("static/assets/data/male_bodycomp_age.json", function(maleData) {
+//   console.log(maleData)
+// });
+
+// Male ///////////////////////////
+// Creating empty arrays and object 
+  male_obj = {};
+  m_event_list = [];
+  m_age_list = [];
+  m_height_list = [];
+  m_weight_list = [];
+
+// Getting Age, Height, Weight and pushing to lists
+  for(var i = 0; i < maleData.length; i++) {
+    m_event_list.push(maleData[i].event)
+    m_age_list.push(maleData[i].age)
+    m_height_list.push((maleData[i].height))
+    m_weight_list.push((maleData[i].weight) * 2.205)}
+   // console.log(m_event_list)
+   // console.log(m_age_list)
+   // console.log(m_height_list)
+   // console.log(m_weight_list)
+
+  // Setting xValues 
+  var xValue = m_event_list;
+
+  // Setting yValues 
+  var yValue1 = m_age_list;
+  var yValue2 = m_height_list;
+  var yValue3 = m_weight_list;
+
+// Making male dictionary
+  var male_obj = {
+    xValue: m_event_list,
+    yValue1: m_age_list,
+    yValue2: m_height_list,
+    yValue3: m_weight_list,
+    textValue1: ("Male Age"),
+    textValue2: ("Male Height"),
+    textValue3: ("Male Weight")}
+  // console.log(male_obj)
+
+  var xValue_male = (male_obj.xValue)
+  var yValue1_male = (male_obj.yValue1)
+  var yValue2_male = (male_obj.yValue2)
+  var yValue3_male = (male_obj.yValue3)
+  var textValue1_male = (male_obj.textValue1)
+  var textValue2_male = (male_obj.textValue2)
+  var textValue3_male = (male_obj.textValue3)
+
+  // Female ///////////////////////////
+  female_obj = {};
+  f_event_list = [];
+  f_age_list = [];
+  f_height_list = [];
+  f_weight_list = [];
+
+// Getting Age, Height, Weight and pushing to lists
+  for(var i = 0; i < femaleData.length; i++) {
+    f_event_list.push(femaleData[i].event)
+    f_age_list.push(femaleData[i].age)
+    f_height_list.push((femaleData[i].height))
+    f_weight_list.push((femaleData[i].weight) * 2.205)}
+   // console.log(m_event_list)
+   // console.log(m_age_list)
+   // console.log(m_height_list)
+   // console.log(m_weight_list)
+
+  // Setting xValues 
+  var xValue = f_event_list;
+
+  // Setting yValues 
+  var yValue1 = f_age_list;
+  var yValue2 = f_height_list;
+  var yValue3 = f_weight_list;
+
+// Making female dictionary
+  var female_obj = {
+    xValue: f_event_list,
+    yValue1: f_age_list,
+    yValue2: f_height_list,
+    yValue3: f_weight_list,
+    // textValue1 : ("Female Age"),
+    // textValue2 : ("Female Height"),
+    // textValue3 : ("Female Weight")
+  }
+
+  var xValue_female = (female_obj.xValue)
+  var yValue1_female = (female_obj.yValue1)
+  var yValue2_female = (female_obj.yValue2)
+  var yValue3_female = (female_obj.yValue3)
+  // var textValue1_female = (female_obj.textValue1)
+  // var textValue2_female = (female_obj.textValue2)
+  // var textValue3_female = (female_obj.textValue3)
+
+// Display default plots - Female
+  function init1() {
+    var barChart1 = {
+      name: "Gold",
+      x: xValue_female,
+      y: yValue1_female,
+      type: 'bar',
+      text: (yValue1_female).map(String),
+      hovertemplate: 'Female Age: %{y}<extra></extra>',
+      opacity: 0.6,
+      marker: {
+        color: 'rgb(218,165,32)'}
+    };
+    var layout1 = {
+      title: {
+        text: "Age",
+        font: {
+          size: 20,
+        }},
+      autosize: true,
+      width: 900,
+      height: 800,
+      margin: {
+        l: 50,
+        r: 50,
+        b: 300,
+        t: 100,
+        pad: 4},
+      xaxis: {
+        categoryorder:'total descending'},
+      yaxis: {
+          range: [0, 35]
+          },
+  };
+  Plotly.newPlot("plot1", [barChart1], layout1);
+}
+  function init2() {
+    var barChart2 = {
+      name: "Gold",
+      x: xValue_female,
+      y: yValue2_female,
+      type: 'bar',
+      text: (yValue2_female).map(String),
+      hovertemplate: 'Female Height: %{y:.1f}<extra></extra>',
+      opacity: 0.6,
+      marker: {
+        color: 'rgb(218,165,32)'}
+    };
+    var layout2 = {
+      title: {
+        text: "Height",
+        font: {
+          size: 20,
+        }},
+      autosize: true,
+      width: 900,
+      height: 800,
+      margin: {
+        l: 50,
+        r: 50,
+        b: 300,
+        t: 100,
+        pad: 4},
+      xaxis: {
+        categoryorder:'total descending'},
+      yaxis: {
+          range: [100, 200]}
+    };
+  Plotly.newPlot("plot2", [barChart2], layout2);
+}
+  function init3() {
+    var barChart3 = {
+      name: "Gold",
+      x: xValue_female,
+      y: yValue3_female,
+      type: 'bar',
+      text: (yValue3_female).map(String),
+      hovertemplate: 'Female Weight: %{y:.2f}<extra></extra>',
+      opacity: 0.6,
+      marker: {
+        color: 'rgb(218,165,32)'},
+    };
+    var layout3 = {
+      title: {
+        text: "Weight",
+        font: {
+          size: 20,
+        }},
+      autosize: true,
+      width: 900,
+      height: 800,
+      margin: {
+        l: 50,
+        r: 50,
+        b: 300,
+        t: 100,
+        pad: 4},
+      xaxis: {
+        categoryorder:'total descending'},
+      yaxis: {
+          range: [0, 350]},
+    };
+  Plotly.newPlot("plot3", [barChart3], layout3);
+}
+init1()
+init2()
+init3()
+
+  // On change to the DOM, call getData()
+ d3.selectAll("#selDataset").on("change", getData);
+  function getData() {
+    function barchart1 () {
+      // Use D1 to select the dropdown menu
+      var dropdownMenu = d3.select("#selDataset");
+      // Assign the value of the dropdown menu option to a variable
+      var dataset = dropdownMenu.property("value");
+
+      var x = [];
+      var y = [];
+      var text = [];
+      var hovertemplate = [];
+
+      if (dataset == 'female') {
+        x = xValue_female;
+        y = yValue1_female;
+        text = (yValue1_female).map(String);
+        hovertemplate = 'Female Age: %{y}<extra></extra>';
+      }
+      if (dataset === 'male') {
+        x = xValue_male;
+        y = yValue1_male;
+        text = (yValue1_male).map(String);
+        hovertemplate = 'Male Age: %{y}<extra></extra>';
+      }
+      Plotly.restyle("plot1", "x", [x]);
+      Plotly.restyle("plot1", "y", [y]);
+      Plotly.restyle("plot1", "text", [text]);
+      Plotly.restyle("plot1", "hovertemplate", [hovertemplate]);
+    }
+    function barchart2 () {
+      // Use D2 to select the dropdown menu
+      var dropdownMenu = d3.select("#selDataset");
+      // Assign the value of the dropdown menu option to a variable
+      var dataset = dropdownMenu.property("value");
+
+      var x = [];
+      var y = [];
+      var text = [];
+      var hovertemplate = [];
+
+      if (dataset == 'female') {
+        x = xValue_female;
+        y = yValue2_female;
+        text = (yValue2_female).map(String);
+        hovertemplate = 'Female Height: %{y:.1f}<extra></extra>';
+      }
+      if (dataset === 'male') {
+        x = xValue_male;
+        y = yValue2_male;
+        text = (yValue2_male).map(String);
+        hovertemplate = 'Male Height: %{y:.1f}<extra></extra>';
+      }
+      Plotly.restyle("plot2", "x", [x]);
+      Plotly.restyle("plot2", "y", [y]);
+      Plotly.restyle("plot2", "text", [text]);
+      Plotly.restyle("plot2", "hovertemplate", [hovertemplate]);
+    }
+
+    function barchart3 () {
+      // Use D3 to select the dropdown menu
+      var dropdownMenu = d3.select("#selDataset");
+      // Assign the value of the dropdown menu option to a variable
+      var dataset = dropdownMenu.property("value");
+
+      var x = [];
+      var y = [];
+      var text = [];
+      var hovertemplate = [];
+
+      if (dataset == 'female') {
+        x = xValue_female;
+        y = yValue3_female;
+        text = (yValue3_female).map(String);;
+        hovertemplate = 'Female Weight: %{y:.2f}<extra></extra>'
+      }
+      if (dataset === 'male') {
+        x = xValue_male;
+        y = yValue3_male;
+        text = (yValue3_male).map(String);
+        hovertemplate = 'Male Weight: %{y:.2f}<extra></extra>';
+      }
+      Plotly.restyle("plot3", "x", [x]);
+      Plotly.restyle("plot3", "y", [y]);
+      Plotly.restyle("plot3", "text", [text]);
+      Plotly.restyle("plot3", "hovertemplate", [hovertemplate]);
+    }
+  barchart1()
+  barchart2()
+  barchart3()
+  }
+
+
+// });
